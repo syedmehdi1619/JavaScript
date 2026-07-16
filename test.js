fe7872a494cf27ca71c4499208b1b870a4070fdf
Syed Mehdi Hassan <syedm8656@gmail.com>
2026-07-16 16:42:10 +0500

    variable done

--- test.js
+++ test.js
@@ -1,1 +1,2 @@
-console.log("hiii")
+console.log('hiii')
+console.log(123)
---
+++ variables.js
@@ -0,0 +1,12 @@
+const myNum = 1234
+let myName = "Mehdi Hassan"
+var myPw = "NotYet"
+myCity = "Karachi"
+let myState
+// myNum = 2222 not possible
+console.log(myNum);
+myName = "Syed Mehdi"
+myPw = "Password"
+myCity = "Islamabad"
+
+console.table([myName, myPw, myCity, myState]);
